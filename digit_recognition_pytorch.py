b197fbde69b7233cc55bf75d6ea99701a94b2d78
Vinay Sawal <vinaysawal@gmail.com>
2019-12-15 09:19:50 -0800

    Pytorch Implementation

--- digit_recognition_pytorch.py
+++ digit_recognition_pytorch.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # for plotting beautiful graphs
 
@@ -13,3 +13,118 @@ from torchvision import transforms, models
 from torch.autograd import Variable
 from torch import nn, optim
 import torch.nn.functional as F
+import os
+
+print(os.listdir("data"))
+
+train = pd.read_csv("data/mnist_dataset.csv", dtype=np.float32)
+train.label.head()
+
+labels = train.label.to_numpy()
+features = train.loc[:, train.columns != 'label'].to_numpy()
+normalized_features = features/255
+#print(features[350:355])
+#print(normalized_features[350:355])
+
+# Split into training and test set
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.2, random_state=42)
+
+# create tensors from numpy arrays
+featuresTrain = torch.from_numpy(features_train)
+labelsTrain = torch.from_numpy(labels_train).type(torch.LongTensor) # data type is long
+featuresTest = torch.from_numpy(features_test)
+labelsTest = torch.from_numpy(labels_test).type(torch.LongTensor) # data type is long
+
+# Set batch size
+batch_size = 256
+
+# Pytorch train and test sets
+train = torch.utils.data.TensorDataset(featuresTrain,labelsTrain)
+test = torch.utils.data.TensorDataset(featuresTest,labelsTest)
+
+# data loader
+train_loader = torch.utils.data.DataLoader(train, batch_size = batch_size, shuffle = True)
+test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = True)
+
+# Create a NN with 5 linear layers
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 5 Hidden Layer Network
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc5 = nn.Linear(64, 10)
+
+        # Dropout module with 0.2 probbability
+        self.dropout = nn.Dropout(p=0.2)
+        # Add softmax on output layer
+        self.log_softmax = F.log_softmax
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        x = self.dropout(F.relu(self.fc4(x)))
+
+        x = self.log_softmax(self.fc5(x), dim=1)
+
+        return x
+
+
+# Instantiate our model
+model = Classifier()
+# Define our loss function
+criterion = nn.NLLLoss()
+# Define the optimier
+optimizer = optim.Adam(model.parameters(), lr=0.0015)
+
+epochs = 25
+steps = 0
+print_every = 50
+train_losses, test_losses = [], []
+
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in train_loader:
+        steps += 1
+        # Prevent accumulation of gradients
+        optimizer.zero_grad()
+        # Make predictions
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
+        #backprop
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+
+            # Turn off gradients for validation
+            with torch.no_grad():
+                model.eval()
+                for images, labels in test_loader:
+                    log_ps = model(images)
+                    test_loss += criterion(log_ps, labels)
+
+                    ps = torch.exp(log_ps)
+                    # Get our top predictions
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+            model.train()
+
+            train_losses.append(running_loss/len(train_loader))
+            test_losses.append(test_loss/len(test_loader))
+
+            print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Training Loss: {:.3f}.. ".format(train_losses[-1]),
+                  "Test Loss: {:.3f}.. ".format(test_losses[-1]),
+                  "Test Accuracy: {:.3f}".format(accuracy/len(test_loader)))
+
+print("Done")
